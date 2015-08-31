a808d1fef66f2a2ea764fd3225eff09ec7d54589
mjvakili <mjvakili@nyu.edu>
2015-08-31 17:39:31 +0000

    turning off baryons for now

--- pkabc/pkabc/eisenstein_hu.py
+++ pkabc/pkabc/eisenstein_hu.py
@@ -30,6 +30,7 @@ def transfnc_eh(k, Om=0.3, h=0.7, T_cmb=2.725, incl_baryons=True):
         # TODO: Fix up this part
 
         # convert k to Mpc^-1 rather than hMpc^-1
+        """
         rk = k * h
         Omh2 = Om * h ** 2
 
@@ -105,4 +106,5 @@ def transfnc_eh(k, Om=0.3, h=0.7, T_cmb=2.725, incl_baryons=True):
 
         // Equation 8
         tk_eh=(om_b/om_m)*tb+(1.-om_b/om_m)*tc;
-
+        """
+        return np.ones((k.shape))
